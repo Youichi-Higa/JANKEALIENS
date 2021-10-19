cb21781143011fb3e09c3e31ca910aaa1896dabd
Youichi-Higa <yo1_1023@yahoo.co.jp>
2021-10-19 14:30:34 +0900

    Vercelでデプロイ

--- pages/test.js
+++ pages/test.js
@@ -4,7 +4,7 @@ const web3 = new Web3(
   "https://eth-rinkeby.alchemyapi.io/v2/yMUx5pobNFkfpdw1irSkIrgXIWoIIIWt"
 );
 
-export default function battle() {
+export default function test() {
   const jankealiensABI = require("../MyNFT.json");
   const jankealiensAddress = "0x94fe135d72c57238df64eb6d4b3e7764b8a1cbbb";
   const jankealiens = new web3.eth.Contract(jankealiensABI, jankealiensAddress);
@@ -14,6 +14,13 @@ export default function battle() {
     .then((uri) => {
       console.log(uri);
     });
+
+  // async function getAccount() {
+  //   const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  //   const account = accounts[0];
+  //   console.log(account);
+  // }
+  // getAccount();
 
   return (
     <>
