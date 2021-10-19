00d212c6707a2f0968600c6b818e176d772293f3
Youichi-Higa <yo1_1023@yahoo.co.jp>
2021-10-19 15:05:40 +0900

    Card componentsの名称変更

--- pages/catalog.js
+++ pages/catalog.js
@@ -1,5 +1,5 @@
 import { Flex } from "@chakra-ui/layout";
-import { Card } from "../components/card";
+import { Card } from "../components/Card";
 import { Header } from "../components/Header";
 
 export default function catalog() {
